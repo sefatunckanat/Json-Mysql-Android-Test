2b4903bf420cc6fe4df09182ab887c9822202e1b
Sefa Tunçkanat <sefatunckanat73@gmail.com>
2015-09-26 09:48:57 +0300

    26.09.2015 09:48

--- JsonMysqlExample/php/config.php
+++
@@ -1,9 +0,0 @@
-<?php 
-	$DB_USER 		= "945704";
-	$DB_PASSWORD	= "password";
-	$DB_DATABASE 	= "945704";
-	$DB_SERVER 		= "localhost";
-	$con 	= 	mysql_connect($DB_SERVER,$DB_USER,$DB_PASSWORD) or die(mysql_error());
-	$db 	=	mysql_select_db($DB_DATABASE);
-	mysql_query("SET NAMES UTF8");
-?>
